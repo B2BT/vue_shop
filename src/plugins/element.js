f4085ff7db224e92f5be38376219e523c2be6b17
yongzx <yongzx@icloud.com>
2022-11-06 21:54:55 +0800

    完成了登录功能

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,11 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Button, Input, Form, FormItem, Message } from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+Vue.use(Message)
+
+// Message需要全局挂载
+Vue.prototype.$message = Message
